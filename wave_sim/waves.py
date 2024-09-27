a96c667b01a36977ed993e12669095beb8922651
Alex Yuji Magnan <amagnan@Mac.localdomain>
2024-09-27 21:32:12 +0900

    more stuff

--- wave_sim/waves.py
+++ wave_sim/waves.py
@@ -2,37 +2,32 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
-# Streamlit title and description
 st.title(":rainbow[Wave sim]")
 
-# Streamlit sliders for user input
+#st sliders for user input
 frequency = st.slider("Select frequency (Hz):", min_value=1, max_value=10, value=5)
 wavelength = st.slider("Select wavelength (m):", min_value=1, max_value=20, value=15)
 #amplitude = st.slider("Select amplitude:", min_value=0.1, max_value=5.0, value=1.0)
-amplitude = 2
+amplitude = 2 #possible to add slider, but for now, just set it to 2
 
 st.divider()
 
-st.write(f"Speed: {frequency * wavelength}")
+st.write(f"Speed: {frequency * wavelength} m/s")
 
 st.divider()
 
-# Duration and sampling rate
-duration = 20  # Duration in seconds or spatial length
-sampling_rate = 1000  # Sampling rate
+distance = 20 
+sampling_rate = 1000 
 
-# Time array
-t = np.linspace(0, duration, int(sampling_rate * duration), endpoint=False)
+t = np.linspace(0, distance, int(sampling_rate * distance), endpoint=False)
 
-# Generate sine wave
+#gen wave
 y = amplitude * np.sin(2 * np.pi * frequency * (t / wavelength))
 
-# Create a DataFrame for the sine wave data
 data = pd.DataFrame({
     'Distance': t,
     'Amplitude': y
 })
 
-# Plotting the sine wave using st.line_chart
 st.line_chart(data.set_index('Distance'), use_container_width=True)
 
